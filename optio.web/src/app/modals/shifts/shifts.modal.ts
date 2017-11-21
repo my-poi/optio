72530ceeb9e0f55cdfd27bf09a601f470ba47339
Maciej Tokarz <maciej.tokarz@my-poi.pl>
2017-11-21 10:23:56 +0100

    splash cd.

--- optio.web/src/app/modals/shifts/shifts.modal.ts
+++ optio.web/src/app/modals/shifts/shifts.modal.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbModalOptions, ModalDismissReasons } from '../../modules/modal/modal.module';
 
 @Component({
   selector: 'app-shifts-modal',
@@ -6,49 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shifts.modal.css']
 })
 export class ShiftsModal implements OnInit {
+  modalOption: NgbModalOptions = {
+    backdrop: 'static',
+    keyboard: false
+  };
+  closeResult: string;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
+  open(content) {
+    this.modalService.open(content, this.modalOption).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
-
-
-// import { Component } from '@angular/core';
-// import {NgbModal, ModalDismissReasons, NgbModalOptions} from '../../modules/modal/modal.module';
-
-// @Component({
-//   selector: 'app-splash-modal',
-//   templateUrl: './splash.modal.html',
-//   styleUrls: ['./splash.modal.css']
-// })
-// export class SplashModal {
-//   modalOption: NgbModalOptions = {};
-//   closeResult: string;
-
-//   constructor(private modalService: NgbModal) {
-//     this.open('A to splash...');
-//    }
-
-//   open(content) {
-//     this.modalOption.backdrop = 'static';
-//     this.modalOption.keyboard = false;
-//     this.modalService.open(content, this.modalOption).result.then((result) => {
-//       this.closeResult = `Closed with: ${result}`;
-//     }, (reason) => {
-//       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-//     });
-//   }
-
-//   private getDismissReason(reason: any): string {
-//     if (reason === ModalDismissReasons.ESC) {
-//       return 'by pressing ESC';
-//     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-//       return 'by clicking on a backdrop';
-//     } else {
-//       return  `with: ${reason}`;
-//     }
-//   }
-
-// }
