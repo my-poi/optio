f5df289ff44762016bf4cc38b8afe0eab3aaac2a
Maciej Tokarz <maciej.tokarz@my-poi.pl>
2017-11-27 13:51:45 +0100

    data na rows

--- optio.api/src/methods/shifts.methods.ts
+++ optio.api/src/methods/shifts.methods.ts
@@ -7,17 +7,17 @@ export class ShiftsMethods {
   constructor(private queries: Queries, private workTimeDatabase: WorkTimeDatabase) { }
 
   async getShifts() {
-    const shiftsData = await this.workTimeDatabase.execute(this.queries.dictionary['select-shifts'], []);
-    const shiftDurationsData = await this.workTimeDatabase.execute(this.queries.dictionary['select-shift-durations'], []);
+    const shiftRows = await this.workTimeDatabase.execute(this.queries.dictionary['select-shifts'], []);
+    const shiftDurationRows = await this.workTimeDatabase.execute(this.queries.dictionary['select-shift-durations'], []);
     const shiftDurations: ShiftDuration[] = [];
     const shifts: Shift[] = [];
 
-    shiftDurationsData.forEach(row => {
+    shiftDurationRows.forEach(row => {
       const shiftDuration = new ShiftDuration(row.shiftId, row.validFrom, row.validTo, row.start, row.finish, row.hours, row.minutes);
       shiftDurations.push(shiftDuration);
     });
 
-    shiftsData.forEach(row => {
+    shiftRows.forEach(row => {
       const durations = shiftDurations.filter(x => x.shiftId === row.id);
       const current = durations.filter(x => !x.validTo)[0] || null;
       const shift = new Shift(row.id, row.sign, row.isValid, durations, current);
