311de4e3fec76db69475c4b479cf1c041b69690d
Maciej Tokarz <maciej.tokarz@my-poi.pl>
2017-11-28 00:09:00 +0100

    korekta zapytań i formatowania daty i czasu

--- optio.api/src/sql/select-shift-durations.sql
+++ optio.api/src/sql/select-shift-durations.sql
@@ -1,1 +1,9 @@
-SELECT * FROM ShiftDurations;
+SELECT   
+  shiftId, 
+  DATE_FORMAT(validFrom, '%Y-%m-%d') as validFrom, 
+  DATE_FORMAT(validTo, '%Y-%m-%d') as validTo, 
+  TIME_FORMAT(start, '%H:%i') as start,
+  TIME_FORMAT(finish, '%H:%i') as finish,
+  hours, 
+  minutes  
+FROM ShiftDurations;
