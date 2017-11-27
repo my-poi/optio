e87ab0925fed40f70ce28cf927a1bd1a1350ea48
Maciej Tokarz <maciej.tokarz@my-poi.pl>
2017-11-27 15:04:04 +0100

    zastosowanie map

--- optio.api/src/methods/shifts.methods.ts
+++ optio.api/src/methods/shifts.methods.ts
@@ -9,19 +9,15 @@ export class ShiftsMethods {
   async getShifts() {
     const shiftRows = await this.workTimeDatabase.execute(this.queries.dictionary['select-shifts'], []);
     const shiftDurationRows = await this.workTimeDatabase.execute(this.queries.dictionary['select-shift-durations'], []);
-    const shiftDurations: ShiftDuration[] = [];
-    const shifts: Shift[] = [];
 
-    shiftDurationRows.forEach(row => {
-      const shiftDuration = new ShiftDuration(row.shiftId, row.validFrom, row.validTo, row.start, row.finish, row.hours, row.minutes);
-      shiftDurations.push(shiftDuration);
+    const shiftDurations: ShiftDuration[] = shiftDurationRows.map(row => {
+      return new ShiftDuration(row.shiftId, row.validFrom, row.validTo, row.start, row.finish, row.hours, row.minutes);
     });
 
-    shiftRows.forEach(row => {
+    const shifts: Shift[] = shiftRows.map(row => {
       const durations = shiftDurations.filter(x => x.shiftId === row.id);
       const current = durations.filter(x => !x.validTo)[0] || null;
-      const shift = new Shift(row.id, row.sign, row.isValid, durations, current);
-      shifts.push(shift);
+      return new Shift(row.id, row.sign, row.isValid, durations, current);
     });
 
     return shifts;
