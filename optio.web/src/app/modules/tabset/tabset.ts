546fd10bea2cecdb295e7946d69f0f734e6db0c7
maciej.tokarz@my-poi.pl <maciej.tokarz@my-poi.pl>
2017-12-04 09:45:11 +0100

    hidden w tabset

--- optio.web/src/app/modules/tabset/tabset.ts
+++ optio.web/src/app/modules/tabset/tabset.ts
@@ -47,6 +47,7 @@ export class NgbTab {
    * Allows toggling disabled state of a given state. Disabled tabs can't be selected.
    */
   @Input() disabled = false;
+  @Input() hidden = false;
 
   @ContentChild(NgbTabContent) contentTpl: NgbTabContent;
   @ContentChild(NgbTabTitle) titleTpl: NgbTabTitle;
@@ -80,7 +81,7 @@ export interface NgbTabChangeEvent {
   exportAs: 'ngbTabset',
   template: `
     <ul [class]="'nav nav-' + type + (orientation == 'horizontal'?  ' ' + justifyClass : ' flex-column')" role="tablist">
-      <li class="nav-item" *ngFor="let tab of tabs">
+      <li class="nav-item" *ngFor="let tab of tabs" [ngStyle]="{'display': tab.hidden ? 'none' : 'block'}">
         <a [id]="tab.id" class="nav-link" [class.active]="tab.id === activeId" [class.disabled]="tab.disabled"
           href (click)="!!select(tab.id)" role="tab" [attr.tabindex]="(tab.disabled ? '-1': undefined)"
           [attr.aria-controls]="(!destroyOnHide || tab.id === activeId ? tab.id + '-panel' : null)"
